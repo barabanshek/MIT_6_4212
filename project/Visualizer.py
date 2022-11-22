0cf4c721ac3c9b4f933d86d4de901476446c11d2
Nikita Lazarev <niklz@csail.mit.edu>
2022-11-22 20:54:32 +0000

    layout generator added

--- project/Visualizer.py
+++ project/Visualizer.py
@@ -14,9 +14,9 @@ class Visualizer:
         self.meshcat_ = meshcat
         self.brick_geom_ = brick_geom
 
-    def visualize_brick(self, brick_geom):
+    def visualize_brick(self, brick_geom, with_margin = True):
         kStep = 0.0075
-        kMargin = 0.02
+        kMargin = 0.02 if with_margin else 0
         points = []
         brick_geom = brick_geom - kMargin
         for i in np.arange(-brick_geom[0]/2, brick_geom[0]/2 + kStep, kStep):
@@ -29,11 +29,14 @@ class Visualizer:
 
         return cloud
 
-    def visualize_bricks(self, bricks, col):
-        brick = self.visualize_brick(self.brick_geom_)
+    def visualize_bricks(self, bricks, col, with_margin = True):
+        brick = self.visualize_brick(self.brick_geom_, with_margin)
         for idx, b in enumerate(bricks):
             self.meshcat_.SetObject("/brick_vis/" + str(idx) + str(col), brick, rgba=col, point_size=0.005)
-            self.meshcat_.SetTransform("/brick_vis/" + str(idx) + str(col), RigidTransform(b))
+
+            #
+            R = RotationMatrix.MakeZRotation(-np.pi/2) if b[3] == 0 else RotationMatrix()
+            self.meshcat_.SetTransform("/brick_vis/" + str(idx) + str(col), RigidTransform(R, b[:3]))
 
     def clear_vis(self):
         self.meshcat_.Delete("/brick_vis")
@@ -46,3 +49,7 @@ class Visualizer:
         # Visualize failed bricks next
         kColRead = Rgba(1, 0.44, 0.33)
         self.visualize_bricks(not_covered_bricks, kColRead)
+
+    def visualize_plan(self, bricks):
+        kColGreen = Rgba(0.61, 1, 0.60)
+        self.visualize_bricks(bricks, kColGreen, False)
